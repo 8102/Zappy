299f3cd0432a6358840faa30a2ca621fa9454138
Hugo Prenat <hugo.prenat@epitech.eu>
2015-06-17 21:26:16 +0200

    Linked list function

--- src/srv/src/init.c
+++ src/srv/src/init.c
@@ -5,10 +5,62 @@
 ** Login   <prenat_h@epitech.eu>
 **
 ** Started on  Wed Jun 17 18:04:44 2015 Hugo Prenat
-** Last update Wed Jun 17 21:20:14 2015 Hugo Prenat
+** Last update Wed Jun 17 21:25:39 2015 Hugo Prenat
 */
 
 #include "zappy.h"
+
+void		add_to_list(t_client **list, t_client *elem)
+{
+  t_client	*it;
+  t_client	*save;
+  bool		stop;
+
+  if (*list == NULL)
+    {
+      elem->next = *list;
+      *list = elem;
+      return ;
+    }
+  stop = true;
+  it = *list;
+  while (it && stop)
+    {
+      if (it->next)
+        it = it->next;
+      else
+        stop = false;
+    }
+  save = it->next;
+  it->next = elem;
+  elem->next = save;
+}
+
+void		delete_from_list(t_client **list, t_client *elem)
+{
+  t_client	*it;
+  t_client	*prev;
+
+  if (!(*list))
+    return ;
+  it = *list;
+  prev = it;
+  while (it)
+    {
+      if (it == elem)
+        {
+          if (prev != it)
+            prev->next = prev->next->next;
+          else
+            *list = (*list)->next;
+          it->next = NULL;
+          return ;
+        }
+      if (it != *list)
+        prev = prev->next;
+      it = it->next;
+    }
+}
 
 size_t		size_team(t_team *teams)
 {
