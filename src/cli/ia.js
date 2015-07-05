46bfb04f1a7248a8e3d9f18b2f9ea71b364418ef
Jean-Baptiste Grégoire <j.baptiste159@gmail.com>
2015-07-05 22:11:20 +0200

    [IA] 	- New ia

--- src/cli/ia.js
+++ src/cli/ia.js
@@ -1,395 +1,448 @@
 module.exports = function (Actions, Ia, Queue) {
-var sleep = require('sleep');
-var spellbook = require('./spellbook.json');
+	var sleep = require('sleep');
+	var spellbook = require('./spellbook.json');
 
-/*
-** debug
-*/
-var debug = false;
+	var debug = true;
 
-var actions = Actions;
-var IA = Ia;
-var cmdQueue = Queue;
+	var actions = Actions;
+	var IA = Ia;
+	var cmdQueue = Queue;
 
-var action = new actions();
+	var action = new actions();
 
-var goal = {
-	NONE: 0,
-	TAKE: 1,
-	MOVE: 2,
-	FIGHT: 3,
-	CAST: 4,
-	SEARCH: 5,
-	WAIT: 6,
-	CALL: 7,
-	EAT: 8,
-	JOIN: 9
-};
 
-var brain = {
-	job: 'collector',
-	inventory: [],
-	hp: 10,
-	teamName: '',
-	teamSize: 1,
-	fov: [],
-	to: {
-		x: 0,
-		y: 0
-	},
-	pos: {
-		x: 0,
-		y: 0
-	},
-	objective: goal.NONE,
-	prevObjective: goal.NONE,
-	obj: undefined,
-	visualCapacity: 1,
-	level: 1,
-	direction: 0
-};
+	var goal = {
+		NONE: 0,
+		EAT: 1,
+		COLLECT: 2,
+		MOVE: 3,
+		CAST: 4,
+		INCANT: 5,
+		JOIN: 6,
+		WAIT: 7
+	}
 
-var entity = {
-	FOOD: 'nourriture',
-	LINEMATE: 'linemate',
-	DERAUMERE: 'deraumere',
-	SIBUR: 'sibur',
-	MENDIANE: 'mendiane',
-	PHIRAS: 'phiras',
-	THYSTAME: 'thystame'
-};
+	var syncFov = false,
+		syncInv = false;
 
-function calcCoord(pos, to) {
-	var tmpRight = 0, tmpLeft = 0;
-	for (var y = 0; y < 10; y++) {
-		if (y * y <= pos && y * y * (y + 2) >= pos) {
-			to.y = y;
-			tmpRight = y * y * (y + 2);
-			tmpLeft = y * y;
-			break;
+	var step = 0;
+
+	var lock = false;
+
+	var brain = {
+		inventory: [],
+		fov: [],
+		pos: {
+			x: 0,
+			y: 0
+		},
+		to: {
+			x: 0,
+			y: 0
+		},
+		objective: goal.NONE,
+		resource: undefined,
+		level: 1,
+		direction: 0,
+		state: undefined,
+		message: {
+			content: undefined,
+			direction: 0
 		}
 	}
-	var middle = (tmpRight + tmpLeft) / 2;
-	to.x = pos - middle;
-}
 
-function dumpFOV() {
-	console.log('======== DUMP FOV ========');
-	console.log(brain.fov);
-	console.log('======== END ========');
-}
-
-function findResource(resource) {
-	if (brain.direction == 4) {
-		for (var i = 0; i < Math.floor((Math.random() * 15) + 1); i++) {
-			if (i % (brain.visualCapacity + 1) == 0) {
-				updateFov();
+	function calcCoord(pos, to) {
+		var tmpRight = 0, tmpLeft = 0;
+		for (var y = 0; y < 10; y++) {
+			if (y * y <= pos && y * y * (y + 2) >= pos) {
+				to.y = y;
+				tmpRight = y * y * (y + 2);
+				tmpLeft = y * y;
+				break;
 			}
-			for (var i = 0; i < brain.fov.length; i++) {
-				if (brain.fov[i].indexOf(resource) > -1) {
-					calcCoord(i, brain.to);
-					brain.objective = goal.TAKE;
-					brain.obj = resource;
-					IA.emit('move');
-					return (true);
-				}
-			}
-			action.forward();
 		}
-		brain.direction = 0;
-	} else {
-		for (var i = 0; i < brain.fov.length; i++) {
-			if (brain.fov[i].indexOf(resource) > -1) {
-				calcCoord(i, brain.to);
-				brain.objective = goal.TAKE;
-				brain.obj = resource;
-				IA.emit('move');
+		var middle = (tmpRight + tmpLeft) / 2;
+		to.x = pos - middle;
+	}
+
+	function inQueue(action) {
+		for (var i = 0; i < cmdQueue.length; i++) {
+			if (action == cmdQueue[i].command) {
 				return (true);
 			}
 		}
-		action.right();
-		updateFov();
-		brain.direction += 1;
-	}
-	return (false);
-}
-
-/*
-** This function returns the quantity of the element passed in parameter
-*/
-function inInventory(stoneName) {
-	var quantity = 0;
-	for (var i = 0; i < brain.inventory.length; i++) {
-		if (brain.inventory[i][0] == stoneName) {
-			quantity += brain.inventory[i][1];
-		}
-	}
-	return (quantity);
-}
-
-function findGoal() {
-	var idx = brain.level + 1;
-	var compos = spellbook[idx.toString()];
-
-	for (var compo in compos) {
-		if (compo == 'nbPlayer') {
-			continue;
-		}
-		if (compos[compo] > 0 && inInventory(compo) < compos[compo]) {
-			brain.objective = goal.SEARCH;
-			brain.obj = compo;
-			return (false);
-		}
-	}
-	console.log(compos);
-	if (compos.nbPlayer > 1) {
-		brain.objective = goal.CALL;
-		IA.emit('callTeam', compos.nbPlayer);
-	} else {
-		prepareIncantation();
-		action.incantation();
-	}
-	return (true);
-}
-
-function updateInventory() {
-	action.inventory();
-}
-
-function updateFov() {
-	brain.fov.length = 0;
-	action.see();
-}
-
-function prepareIncantation() {
-	var idx = brain.level + 1;
-	var compos = spellbook[idx.toString()];
-
-	for (var compo in compos) {
-		if (compo == 'nbPlayer') {
-			continue;
-		}
-		if (compos[compo] > 0) {
-			var quantity = compos[compo];
-			while (quantity > 0) {
-				action.dropItem(compo);
-				quantity -= 1;
-			}
-		}		
-	}
-}
-
-IA.on('levelUp', function() {
-	console.log('I level ' + (brain.level + 1) + ' now madafaka !');
-	brain.level += 1;
-	brain.visualCapacity += 1;
-	brain.objective = goal.NONE;
-});
-
-IA.on('callTeam', function(playersRequired) {
-	var currentCase = brain.fov[0];
-	var nbPlayer = 0;
-
-	if (currentCase == undefined) {
 		return (false);
 	}
-	for (var i = 0; i < currentCase.length; i++) {
-		if (currentCase[i] == 'joueur') {
-			nbPlayer += 1;
-		}
-	}
-	if (nbPlayer >= playersRequired) {
-		brain.objective = goal.CALL;
-		prepareIncantation();
-		action.incantation();
-	} else {
-		action.sayToEveryone('incantation-' + brain.level);
-	}
-	return (true);
-});
 
-IA.on('notification', function(msg, direction) {
-	if (brain.objective != goal.WAIT && brain.objective != goal.EAT && msg.split('-')[0] == 'incantation' && msg.split('-')[1] == brain.level) {
-		brain.pos.x = brain.pos.y = 0;
-		if (direction == 1 || direction == 2 || direction == 8) {
-			brain.pos.y = 1;
-		} else if (direction >= 4 && direction <= 6) {
-			brain.pos.y = -1;
+	function updateInventory() {
+		if (!inQueue('inventaire') && cmdQueue.length < 10) {
+			action.inventory();
+			syncInv = false;
 		}
-		if (direction >= 3 && direction <= 4) {
-			brain.pos.x = 1;
-		} else if (direction >= 6 && direction <= 8) {
-			brain.pos.x = -1;
-		}
-		if (brain.pos.y < 0) {
-			action.right();
-			action.right();
-		}
-		brain.objective = goal.JOIN;
 	}
-});
 
-IA.on('saw', function(cmd) {
-	cmd = cmd.replace('{', '');
-	cmd = cmd.replace('}', '');
-	brain.fov.length = 0;
-	tmp = cmd.split(',');
-	for (var j = 0; j < tmp.length; j++) {
-		brain.fov.push(tmp[j].split(' '));
+	function updateFov() {
+		if (!inQueue('voir') && cmdQueue.length < 10) {
+			action.see();
+			syncFov = false;
+		}
 	}
-});
 
-IA.on('updateInventory', function(action, compoName, quantity) {
-	if (action == 'prend') {
-		var added = false;
+	function inInventory(stoneName) {
+		var quantity = 0;
 		for (var i = 0; i < brain.inventory.length; i++) {
-			if (brain.inventory[i][0] == compoName) {
-				brain.inventory[i][1] += quantity;
-				added = true;
+			if (brain.inventory[i][0] == stoneName) {
+				quantity += brain.inventory[i][1];
 			}
 		}
-		if (!added) {
-			brain.inventory.push([compoName, quantity]);
+		return (quantity);
+	}
+
+	function findRessource(resource) {
+		if (!syncFov) {
+			return (false);
 		}
-		brain.objective = goal.NONE;
-		if (compoName == 'nourriture') {
-			brain.hp += quantity;
-		}
-	} else {
-		for (var i = 0; i < brain.inventory.length; i++) {
-			if (brain.inventory[i][0] == compoName) {
-				brain.inventory[i][1] -= 1;
+		// console.log(brain.fov);
+		for (var i = 0; i < brain.fov.length; i++) {
+			if (brain.fov[i].indexOf(resource) > -1) {
+				brain.pos.x = brain.pos.y = 0;
+				calcCoord(i, brain.to);
+				brain.resource = resource;
+				brain.objective = goal.MOVE;
+				brain.state = 'take';
+				return (true);
 			}
-			if (brain.inventory[i][1] == 0) {
-				brain.inventory.splice(i, 1);
-			}
 		}
-		brain.objective = goal.NONE;
+		return (false);
 	}
-	updateFov();
-});
 
-IA.on('inventory', function(cmd) {
-	brain.inventory.length = 0;
-	cmd = cmd.replace('{', '');
-	cmd = cmd.replace('}', '');
-	tmp = cmd.split(',');
-	for (var j = 0; j < tmp.length; j++) {
-		brain.inventory.push(tmp[j].split(' '));
-	}
-});
-
-IA.on('bump', function() {
-	updateFov();
-	updateInventory();
-});
-
-IA.on('update', function(part) {
-	if (part == 'prend' || part == 'pose') {
-		updateFov();
-	} else {
-		updateInventory();
-	}
-});
-
-IA.on('move', function() {
-	if (brain.pos.y != brain.to.y) {
-		action.forward();
-		brain.pos.y += 1;
-		return (true);
-	}
-	if (brain.to.x < 0) {
-		action.left();
-	} else if (brain.to.x > 0 ){
-		action.right();
-	}
-	if (brain.pos.x != brain.to.x) {
-		action.forward();
+	function move() {
+		if (cmdQueue.length > 0) {
+			return (false);
+		}
+		console.log('je suis en ' + brain.pos.x + ' ' + brain.pos.y);
+		console.log('je dois aller en ' + brain.to.x + ' ' + brain.to.y);
+		if (brain.pos.y != brain.to.y) {
+			action.forward();
+			brain.pos.y += 1;				
+			return (true);
+		}
 		if (brain.to.x < 0) {
-			brain.pos.x -= 1;
+			action.left();
+		} else if (brain.to.x > 0 ) {
+			action.right();
+		}
+		if (brain.pos.x != brain.to.x) {
+			action.forward();
+			if (brain.to.x < 0) {
+				brain.pos.x -= 1;
+			} else {
+				brain.pos.x += 1;
+			}
+			return (true);
+		}
+		if (brain.state == 'take') {
+			action.takeItem(brain.resource);
+			brain.pos.x = brain.pos.y = brain.to.x = brain.to.y = 0;
+			brain.state = 'took';
+			brain.objective = goal.NONE;
+			brain.resource = undefined;
+			syncInv = false;
+			syncFov = false;
+		} else if (brain.state == 'join') {
+			brain.objective = goal.NONE;
+			console.log('jy suis ! ' + brain.pos.x + ' ' + brain.pos.y);
+			brain.pos.x = brain.pos.y = brain.to.x = brain.to.y = 0;
+			brain.state = 'joined';
+		}
+	}
+
+	function collect(resource) {
+		if (!syncFov) {
+			return (false);
+		}
+		brain.resource = resource;
+		var dir = Math.floor((Math.random() * 4) + 1);
+		if (brain.direction == dir) {
+			var steps = Math.floor((Math.random() * 6) + 1);
+			for (var i = 0; i < steps; i++) {
+				action.forward();
+			}
+			brain.direction = 0;
+		}
+		if (findRessource(resource)) {
+			// object trouvé plus qu'à aller le chercher
+			move();
 		} else {
-			brain.pos.x += 1;
+			brain.direction = (brain.direction + 1) % 4;
+			action.right();
+			syncFov = false;
+		}
+	}
+
+	function findGoal() {
+		var idx = brain.level + 1;
+		var compos = spellbook[idx.toString()];
+
+		if (!syncFov || !syncInv) {
+			return (false);
+		}
+		for (var compo in compos) {
+			if (compo == 'nbPlayer') {
+				continue;
+			}
+			if (compos[compo] > 0 && inInventory(compo) < compos[compo]) {
+				brain.objective = goal.COLLECT;
+				brain.resource = compo;
+				return (false);
+			}
+		}
+		if (compos.nbPlayer > 1) {
+			brain.objective = goal.CALL;
+		} else {
+			brain.objective = goal.CAST;
 		}
 		return (true);
 	}
-	action.takeItem(brain.obj);
-	brain.objective = goal.NONE;
-	brain.obj = undefined;
-	brain.pos.x = brain.pos.y = 0;
-});
 
-IA.on('starving', function() {
-	findResource(entity.FOOD);		
-});
+	function onCase(compo) {
+		if (!syncFov) {
+			return (0);
+		}
+		var quantity = 0;	
+		var currentCase = brain.fov[0];
 
-IA.on('launch', function() {
-	run();
-});
-
-IA.on('wait', function() {
-	if (brain.objective != goal.WAIT) {
-		brain.prevObjective = brain.objective;
-		brain.objective = goal.WAIT;
+		if (currentCase.length == 0) {
+			return (false);
+		}
+		for (var i = 0; i < currentCase.length; i++) {
+			if (currentCase[i] == compo) {
+				quantity += 1;
+			}
+		}
+		return (quantity);
 	}
-});
 
-IA.on('wakeUp', function() {
-	if (brain.objective == goal.WAIT) {
-		brain.objective = brain.prevObjective;
-		brain.prevObjective = goal.NONE;
-	}
-});
+	function prepareIncantation() {
+		var idx = brain.level + 1;
+		var compos = spellbook[idx.toString()];
+		var drop = false;
 
-function brainManagement() {
-	if (brain.objective == goal.NONE) {
-		updateFov();
+		if (!syncFov || !syncInv) {
+			return (false);
+		}
+		if (onCase('joueur') > compos.nbPlayer) {
+			console.log('----> ' + onCase('joueur') + ' ' + compos.nbPlayer);
+			if (cmdQueue.length == 10) {
+				return (false);
+			}
+			action.forward();
+			syncFov = false;
+			return (false);
+		}
+		if (brain.hp <= 8) {
+			brain.objective = goal.EAT;
+			return (false);
+		}
+		for (var compo in compos) {
+			if (compo == 'nbPlayer') {
+				continue;
+			}
+			if (compos[compo] < onCase(compo) && brain.fov[0].indexOf(compo) > -1) {
+				if (cmdQueue.length < 10) {
+					action.takeItem(compo);
+					syncFov = syncInv = false;
+				}
+				drop = true;
+			} else if (compos[compo] > 0 && inInventory(compo) > 0 && compos[compo] > onCase(compo) ) {
+				var quantity = compos[compo];
+				while (quantity > 0) {
+					action.dropItem(compo);
+					syncFov = syncInv = false;
+					quantity -= 1;
+				}
+				drop = true;
+			}
+		}
+		if (!drop) {
+			brain.objective = goal.INCANT;
+		}
+		return (!drop);
 	}
-	if (brain.objective == goal.NONE && brain.hp < 5) {
-		brain.objective = goal.EAT;
+
+	function incant() {
+		if (!inQueue('incantation')) {
+			action.incantation();
+		}
 	}
-	if (brain.objective == goal.EAT && brain.hp > 5) {
+
+	function callTeam() {
+		var currentCase = brain.fov[0];
+		var nbPlayer = 0;
+		var idx = brain.level + 1;
+		var playersRequired = spellbook[idx.toString()].nbPlayer;
+
+		if (!syncFov || currentCase == undefined) {
+			return (false);
+		}
+		for (var i = 0; i < currentCase.length; i++) {
+			if (currentCase[i] == 'joueur') {
+				nbPlayer += 1;
+			}
+		}
+		if (nbPlayer == 0) {
+			return (false);
+		}
+		console.log(nbPlayer + '/' + playersRequired + ' players');
+		if (nbPlayer >= playersRequired) {
+			brain.objective = goal.CAST;
+		} else {
+			action.sayToEveryone('incantation-' + brain.level);
+			syncFov = false;
+		}
+		return (true);	
+	}
+
+	function joinCoop() {
+		if (brain.state != 'joined') {
+			move();
+		}
+	}
+
+	function brainManagement() {
+		if (!syncFov) {
+			updateFov();
+		}
+		if (!syncInv) {
+			updateInventory();
+		}
+		if (brain.objective == goal.NONE && brain.hp < 10) {
+			brain.objective = goal.EAT;
+		}
+		switch (brain.objective) {
+			case goal.EAT:
+				collect('nourriture');
+				break;
+			case goal.COLLECT:
+				collect(brain.resource);
+				break;
+			case goal.MOVE:
+				move();
+				break;
+			case goal.NONE:
+				findGoal();
+				break;
+			case goal.CALL:
+				callTeam();
+				break;
+			case goal.CAST:
+				prepareIncantation();
+				break;
+			case goal.INCANT:
+				incant();
+				break;
+			case goal.JOIN:
+				joinCoop();
+				break;
+			case goal.WAIT:
+				break;
+			default:
+				console.log('pas d\'objectif... ' + brain.objective);
+		}
+	}
+
+	IA.on('forward', function() {
+		step += 1;
+		if (step % (brain.level + 1) == 0) {
+			step = 0;
+			updateFov();
+		}
+	});
+
+	IA.on('saw', function(cmd) {
+		syncFov = true;
+		cmd = cmd.replace('{', '');
+		cmd = cmd.replace('}', '');
+		brain.fov.length = 0;
+		tmp = cmd.split(',');
+		for (var j = 0; j < tmp.length; j++) {
+			brain.fov.push(tmp[j].split(' '));
+		}
+		if (brain.objective == goal.COLLECT || brain.objective == goal.EAT) {
+			if (findRessource(brain.resource)) {
+				// objet trouvé plus qu'à aller le chercher !
+				move();
+			}
+		}
+	});
+
+	IA.on('inventory', function(cmd) {
+		brain.inventory.length = 0;
+		cmd = cmd.replace('{', '');
+		cmd = cmd.replace('}', '');
+		tmp = cmd.split(',');
+		for (var j = 0; j < tmp.length; j++) {
+			brain.inventory.push(tmp[j].split(' '));
+		}
+		brain.hp = brain.inventory[0][1];
+		syncInv = true;
+	});
+
+	IA.on('notification', function(msg, direction) {
+		if (!lock) {
+			lock = true;
+			brain.objective = goal.WAIT;
+		}
+		if (brain.state == 'join' || (brain.objective == goal.WAIT && cmdQueue.length > 0)) {
+			return (false);
+		}
+		if (brain.objective != goal.EAT && msg.split('-')[0] == 'incantation' && msg.split('-')[1] == brain.level) {
+			brain.pos.x = brain.pos.y = brain.to.x = brain.to.y = 0;
+			if (direction == 1 || direction == 2 || direction == 8) {
+				brain.to.y = 1;
+			} else if (direction >= 4 && direction <= 6) {
+				brain.to.y = -1;
+			}
+			if (direction >= 2 && direction <= 4) {
+				brain.to.x = -1;
+			} else if (direction >= 6 && direction <= 8) {
+				brain.to.x = 1;
+			}
+			if (brain.to.y < 0) {
+				if (cmdQueue.length > 8) {
+					console.log('je quitte la biatch !!');
+					return (false);
+				}
+				action.right();
+				action.right();
+				brain.to.x *= -1;
+				brain.to.y *= -1;
+			}
+			console.log('le son vient de ' + direction + ' je dois donc me rendre en ' + brain.to.x + ' ' + brain.to.y);
+			brain.message.content = msg;
+			brain.message.direction = direction;
+			brain.objective = goal.MOVE;
+			brain.state = 'join';
+		}
+	});
+
+	IA.on('levelUp', function() {
+		console.log('I level ' + (brain.level + 1) + ' now madafaka !');
+		brain.level += 1;
 		brain.objective = goal.NONE;
+	});
+
+	IA.on('update', function() {
+		syncFov = syncInv = false;
+	});
+
+	IA.on('launch', function() {
+		run();
+	});
+
+	function run() {
+		if (debug) console.log('Here we gooo !');
+		var intervall = setInterval(brainManagement, 50)
 	}
-	switch (brain.objective) {
-		case goal.TAKE:
-			IA.emit('move');
-			break;
-		case goal.EAT:
-			IA.emit('starving');
-			break;
-		case goal.MOVE:
-			IA.emit('move');
-			break;
-		case goal.JOIN:
-			// IA.emit('notification');
-			break;
-		case goal.SEARCH:
-			findResource(brain.obj);
-			break;
-		case goal.WAIT:
-			//IA.emit('');
-			break;
-		case goal.CALL:
-			IA.emit('callTeam');
-			break;
-		case goal.NONE:
-			findGoal();
-			break;
-		default:
-			console.log('im waiting');
-	}
+
 }
-
-function run() {
-	if (debug) console.log('Here we gooo !');
-	var intervall = setInterval(brainManagement, 1000);
-}
-
-};
-
-/*
-**		 Todo:
-**			- modifier l'update de l'inventaire
-**			- update brain.hp
-**			- enlever les commandes qui ont échoué de la queue (ex: avance --> level actuel : 2)
-*/
